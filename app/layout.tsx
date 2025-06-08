4525cc1bc0d4d64387a5a627056323f9d21698be
cldrojas <cld.rojasz@gmail.com>
2025-06-08 09:57:24 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Daily Budget',
-  description: 'Track your daily budget and savings'
+  description: 'Track your daily budget and savings',
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png']
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
