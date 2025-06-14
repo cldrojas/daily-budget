9098a4626ea599d5c731eb237103c00e28741acf
cldrojas <cld.rojasz@gmail.com>
2025-06-14 19:52:49 -0400

    add overview section

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+  // adapter: cloudflare({
+  //   platformProxy: {
+  //     enabled: true
+  //   }
+  // }),
   vite: {
     plugins: [tailwindcss()],
   }
